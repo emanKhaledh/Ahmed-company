13489a4dd42dc451ea5effdd4f0dc13be49b0f47
eman <eman_h_2009@live.com>
2019-11-10 00:12:06 +0200

    serve

--- server/app.js
+++ server/app.js
@@ -1,14 +1,28 @@
 const express = require('express');
+
 const app = express();
+const favicon = require('express-favicon');
+
+const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
+const path = require('path');
 const controllers = require('./controllers');
-const path = require('path')
+require('env2')('./server/.env');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('port', process.env.PORT || 9000);
+app.set('port', port);
+
 app.use(controllers);
-app.use(express.static(path.join(__dirname, '..', 'client', 'build'))); 
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html')) })
+
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+
+
+
 
 module.exports = app;
